8a46cec6d5fb31324164cc18afcf55bb4152dea6
Paulo Henrique <paulo.scape@gmail.com>
2020-11-20 15:26:14 -0300

    add external dependency component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
+import Modal from "host/Modal";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => (
+  <div>
+    Hi there, I'm the consumer application and I'm consuming {`Modal`} from the
+    host dynamically.
+    <div>
+      <Modal>Hmmm</Modal>
+    </div>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("app"));
